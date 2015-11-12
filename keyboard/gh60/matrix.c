95a49d6b023ee511afc12cc3051476c2972ff236
Cory Beutler <cg.beutler@gmail.com>
2015-11-12 00:01:57 -0700

    Modified the gh60 keyboard to be the gambler

--- keyboard/gh60/matrix.c
+++ keyboard/gh60/matrix.c
@@ -141,35 +141,44 @@ uint8_t matrix_key_count(void)
  */
 static void  init_cols(void)
 {
-    // Input with pull-up(DDR:0, PORT:1)
-    DDRF  &= ~(1<<0 | 1<<1);
-    PORTF |=  (1<<0 | 1<<1);
-    DDRE  &= ~(1<<6);
-    PORTE |=  (1<<6);
-    DDRD  &= ~(1<<7 | 1<<6 | 1<<4);
-    PORTD |=  (1<<7 | 1<<6 | 1<<4);
-    DDRC  &= ~(1<<7 | 1<<6);
-    PORTC |=  (1<<7 | 1<<6);
-    DDRB  &= ~(1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
-    PORTB |=  (1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
+//    // Input with pull-up(DDR:0, PORT:1)
+//    DDRF  &= ~(1<<0 | 1<<1);
+//    PORTF |=  (1<<0 | 1<<1);
+//    DDRE  &= ~(1<<6);
+//    PORTE |=  (1<<6);
+//    DDRD  &= ~(1<<7 | 1<<6 | 1<<4);
+//    PORTD |=  (1<<7 | 1<<6 | 1<<4);
+//    DDRC  &= ~(1<<7 | 1<<6);
+//    PORTC |=  (1<<7 | 1<<6);
+//    DDRB  &= ~(1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
+//    PORTB |=  (1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
+    DDRF  &= ~(1<<0 | 1<<1 | 1<<4);
+    PORTF |=  (1<<0 | 1<<1 | 1<<4);
+    DDRB  &= ~(1<<0 | 1<<1 | 1<<2);
 }
 
 static matrix_row_t read_cols(void)
 {
+//    return (PINF&(1<<0) ? 0 : (1<<0)) |
+//           (PINF&(1<<1) ? 0 : (1<<1)) |
+//           (PINE&(1<<6) ? 0 : (1<<2)) |
+//           (PINC&(1<<7) ? 0 : (1<<3)) |
+//           (PINC&(1<<6) ? 0 : (1<<4)) |
+//           (PINB&(1<<6) ? 0 : (1<<5)) |
+//           (PIND&(1<<4) ? 0 : (1<<6)) |
+//           (PINB&(1<<1) ? 0 : (1<<7)) |
+//           ((PINB&(1<<0) && PINB&(1<<7)) ? 0 : (1<<8)) |   // Rev.A and B
+//           (PINB&(1<<5) ? 0 : (1<<9)) |
+//           (PINB&(1<<4) ? 0 : (1<<10)) |
+//           (PIND&(1<<7) ? 0 : (1<<11)) |
+//           (PIND&(1<<6) ? 0 : (1<<12)) |
+//           (PINB&(1<<3) ? 0 : (1<<13));
     return (PINF&(1<<0) ? 0 : (1<<0)) |
            (PINF&(1<<1) ? 0 : (1<<1)) |
-           (PINE&(1<<6) ? 0 : (1<<2)) |
-           (PINC&(1<<7) ? 0 : (1<<3)) |
-           (PINC&(1<<6) ? 0 : (1<<4)) |
-           (PINB&(1<<6) ? 0 : (1<<5)) |
-           (PIND&(1<<4) ? 0 : (1<<6)) |
+           (PINF&(1<<4) ? 0 : (1<<2)) |
+           (PINB&(1<<0) ? 0 : (1<<5)) |
            (PINB&(1<<1) ? 0 : (1<<7)) |
-           ((PINB&(1<<0) && PINB&(1<<7)) ? 0 : (1<<8)) |     // Rev.A and B
-           (PINB&(1<<5) ? 0 : (1<<9)) |
-           (PINB&(1<<4) ? 0 : (1<<10)) |
-           (PIND&(1<<7) ? 0 : (1<<11)) |
-           (PIND&(1<<6) ? 0 : (1<<12)) |
-           (PINB&(1<<3) ? 0 : (1<<13));
+           (PINB&(1<<2) ? 0 : (1<<9));
 }
 
 /* Row pin configuration
@@ -179,8 +188,12 @@ static matrix_row_t read_cols(void)
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRD  &= ~0b00101111;
-    PORTD &= ~0b00101111;
+//    DDRD  &= ~0b00101111;
+//    PORTD &= ~0b00101111;
+    DDRD  &= ~0b00110111;
+    PORTD &= ~0b00110111;
+    DDRB  &= ~0b01110000;
+    PORTB &= ~0b01110000;
 }
 
 static void select_row(uint8_t row)
@@ -200,10 +213,22 @@ static void select_row(uint8_t row)
             PORTD &= ~(1<<2);
             break;
         case 3:
-            DDRD  |= (1<<3);
-            PORTD &= ~(1<<3);
+            DDRB  |= (1<<4);
+            PORTB &= ~(1<<4);
             break;
         case 4:
+            DDRB  |= (1<<5);
+            PORTB &= ~(1<<5);
+            break;
+        case 5:
+            DDRB  |= (1<<6);
+            PORTB &= ~(1<<6);
+            break;
+        case 6:
+            DDRD  |= (1<<4);
+            PORTD &= ~(1<<4);
+            break;
+        case 7:
             DDRD  |= (1<<5);
             PORTD &= ~(1<<5);
             break;
